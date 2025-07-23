fc8530caf169ebca336b37e4b5dc3e109c1e8260
RachitSinghh <singhrachit@outlook.in>
2025-07-23 16:58:08 +0530

    feat: MVP of cryptic

--- App.tsx
+++ App.tsx
@@ -1,28 +1,160 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { Platform, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import * as Yup from "yup";
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
+import { Formik } from 'formik';
+import { ImageBackground } from 'react-native/types_generated/index';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+const passwordSchema = Yup.object().shape({
+  passwordLength: Yup.number()
+    .min(4, 'Should be minimum of 4 characters')
+    .max(16, 'should be max of 16 characters')
+    .required('Length is required')
 });
 
-export default App;
+
+const App = () => {
+
+  const [password, setPassword] = useState('');
+  const [isPassGenerated, SetIsPassGenerated] = useState(false)
+
+  const [lowerCase, setLowerCase] = useState(true)
+  const [upperCase, setUpperCase] = useState(false)
+  const [numbers, setNumbers] = useState(false)
+  const [symbols, setSymbols] = useState(false)
+
+  const generatePasswordString = (passwordLength: number) => {
+    //
+    let characterList = '';
+
+    const upperCaseChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerCaseChar = upperCaseChar.toLowerCase();
+    //  console.log(lowerCaseChar);
+    const digitChars = '0123456789';
+    const specialChars = '!@#$%^&*()_+';
+
+    if (upperCase) {
+      characterList += upperCaseChar;
+    }
+    if (lowerCase) {
+      characterList += lowerCaseChar;
+    }
+
+    if (numbers) {
+      characterList += digitChars;
+    }
+
+    if (symbols) {
+      characterList += specialChars;
+    }
+
+
+    const passwordResult = createPassword(characterList, passwordLength)
+
+    setPassword(passwordResult);
+    SetIsPassGenerated(true);
+  }
+
+  const createPassword = (characters: string, paswordLength: number) => {
+    //
+    let result = '';
+    for (let i = 0; i < paswordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
+  }
+
+  const resetPasswordState = () => {
+    //
+    setPassword('')
+    SetIsPassGenerated(false)
+    setLowerCase(true)
+    setUpperCase(false)
+    setNumbers(false)
+    setSymbols(false)
+  }
+
+  return (
+    <ScrollView keyboardShouldPersistTaps='handled'>
+      <SafeAreaView style={[styles.androidSafeAreaView, styles.container]}>
+        <View style={styles.formContainer}>
+          <Text style={styles.container}>Password Generator</Text>
+          <Formik
+            initialValues={{ passwordLength: '' }}
+            validationSchema={passwordSchema}
+            onSubmit={values => {
+              console.log(values)
+              generatePasswordString(Number(values.passwordLength))
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              isValid,
+              handleChange,
+              handleSubmit,
+              isSubmitting,
+              handleReset,
+              /* and other goodies */
+            }) => (
+              <>
+                <View style={styles.inputWrapper}>
+                  <View style={styles.inputColumn}>
+                    <Text style={styles.heading}>Password Length</Text>
+                    {touched.passwordLength && errors.passwordLength && (
+                      <Text style={styles.errorText}>{errors.passwordLength}</Text>
+                    )}
+                  </View>
+                    <TextInput
+                      style={styles.inputStyle}
+                      value={values.passwordLength}
+                      onChangeText={handleChange('passwordLength')}
+                      placeholder='Ex.8'
+                      keyboardType='numeric'
+                    />
+                </View>
+                <View style={styles.inputWrapper}>
+                  <Text style={styles.heading}>Include lowercase</Text>
+                  <BouncyCheckbox 
+                    disableBuiltInState
+                    isChecked={lowerCase}
+                    onPress={() => setLowerCase(!lowerCase)}
+                      fillColor='#29AB87'
+                  />
+                </View>
+                <View style={styles.inputWrapper}></View>
+                <View style={styles.inputWrapper}></View>
+                <View style={styles.inputWrapper}></View>
+                <View style={styles.formActions}>
+                  <TouchableOpacity>
+                    <Text>Generate Password</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                    <Text>Reset</Text>
+                  </TouchableOpacity>
+                </View>
+
+              </>
+            )}
+          </Formik>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  androidSafeAreaView: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? 20 : 0
+  },
+  container: {
+
+  },
+  formContainer: {}
+})
---
+++ package.md
@@ -0,0 +1,3 @@
+// npm i Using Yup for validation 
+// npm i formik
+// npm i react-native-bouncy-checkbox 
